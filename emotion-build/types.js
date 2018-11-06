41e64176f622153795d8019536e3de560208ed28
mitchellhamilton <mitchell@hamil.town>
2018-11-06 14:08:40 +1000

    Copy emotion build system here

---
+++ emotion-build/types.js
@@ -0,0 +1,13 @@
+// @flow
+
+/*::
+
+export type Package = {
+  configs: Array<{ config: Object, outputConfigs: Array<Object> }>,
+  path: string,
+  name: string,
+  pkg: Object,
+  input: string
+}
+
+*/
