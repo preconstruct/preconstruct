41122a51f829bbf4d901f6b1bca1fed9e7d1709a
mitchellhamilton <mitchell@hamil.town>
2018-11-07 20:35:38 +1000

    Remove an extra space

--- src/logger.js
+++ src/logger.js
@@ -5,7 +5,7 @@ import { Package } from "./package";
 let preconstructEmoji = "🎁 ";
 
 function suffix(pkg?: Package) {
-  return pkg !== undefined ? ` ${pkg.name} ` : " ";
+  return pkg !== undefined ? ` ${pkg.name}` : "";
 }
 
 export function error(error: string, pkg?: Package) {
