294d590a9fe65777d71014ccb0a6d29e8cbe552f
mitchellhamilton <mitchell@hamil.town>
2018-11-18 13:56:30 +1000

    Add other alias formats

--- src/index.js
+++ src/index.js
@@ -7,14 +7,28 @@
 import { Package } from "./package";
 import { getAliases } from "./build/aliases";
 
+function getPackages(cwd) {
+  let pkg = Package.createSync(cwd);
+  let pkgs = pkg.packagesSync();
+  if (pkgs === null) {
+    pkgs = [pkg];
+  }
+  pkgs = pkgs.map(x => x.strict());
+  return pkgs;
+}
+
+// inspired by https://github.com/Andarist/lerna-alias
 export let aliases = {
   jest(cwd: string = process.cwd()) {
-    let pkg = Package.createSync(cwd);
-    let pkgs = pkg.packagesSync();
-    if (pkgs === null) {
-      pkgs = [pkg];
-    }
-    pkgs = pkgs.map(x => x.strict());
+    let pkgs = getPackages(cwd);
     return getAliases(pkgs, name => `^${name}$`);
+  },
+  rollup(cwd: string = process.cwd()) {
+    let pkgs = getPackages(cwd);
+    return getAliases(pkgs);
+  },
+  webpack(cwd: string = process.cwd()) {
+    let pkgs = getPackages(cwd);
+    return getAliases(pkgs, name => `$${name}$`);
   }
 };
