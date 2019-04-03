69e5d4e3e72495a200e54fb73db047fc00499ca1
mitchellhamilton <mitchell@hamil.town>
2019-04-03 14:00:49 +1000

    stuff

---
+++ src/rollup-plugins/flow-and-prod-dev-entry.js
@@ -0,0 +1,30 @@
+// @flow
+import path from "path";
+import type { Plugin } from "./types";
+
+export default function flowEntry(): Plugin {
+  return {
+    name: "flow-and-prod-dev-entry",
+
+    generateBundle(opts, bundle, something) {
+      const outDir = opts.dir;
+
+      for (const n in bundle) {
+        const file = bundle[n];
+        if (file.isAsset || !file.isEntry || file.facadeModuleId == null) {
+          continue;
+        }
+
+        let relativeToSource = path.relative(opts.dir, file.facadeModuleId);
+        `// @flow
+          export * from "${relativeToSource}";${
+          flowMode === "all"
+            ? `\nexport { default } from "${relativeToSource}";`
+            : ""
+        }\n`;
+        const fileName = file.fileName + ".flow";
+        bundle[fileName] = { fileName, isAsset: true, source };
+      }
+    }
+  };
+}
