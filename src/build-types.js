8eb9cf2c7fdda98527df90063196de0516324d43
mitchellhamilton <mitchell@hamil.town>
2019-04-07 09:16:17 +1000

    WIP

---
+++ src/build-types.js
@@ -0,0 +1,52 @@
+// @flow
+
+type BuildType = "required" | "prompted" | "optional";
+
+type FieldType = "single" | "mapping";
+
+type SingleField = {
+  name: string,
+  type: "single",
+  suffix: string
+};
+
+type Field =
+  | SingleField
+  | {
+      name: string,
+      type: "mapping",
+      suffix: string,
+      bases: Array<SingleField>
+    };
+
+type BaseBuild = {
+  Field: string,
+  getValid: (pkgName: string) => { [key: string]: string } | string
+};
+
+type BasePromptBuild = BaseBuild & { prompt: string };
+
+type RequiredBuild = BasePromptBuild & {
+  type: "required"
+};
+
+type PromptedBuild = BasePromptBuild & {
+  type: "prompted"
+};
+
+type OptionalBuild = BaseBuild & {
+  type: "optional"
+};
+
+export type Build = RequiredBuild | PromptedBuild | OptionalBuild;
+
+export let builds: Array<Build> = [
+  {
+    field: "main",
+    type: "required",
+    getValid: pkgName => {
+      return `${pkgName}.`;
+    },
+    prompt: ""
+  }
+];
