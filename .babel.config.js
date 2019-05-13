7c05d124480e4383537bc27107463ccf8a35a4bc
mitchellhamilton <mitchell@hamil.town>
2019-05-13 17:08:07 +1000

    Start

---
+++ .babel.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        targets: { node: 8 }
+      }
+    ],
+    "@babel/preset-flow"
+  ],
+  plugins: ["@babel/plugin-proposal-class-properties"]
+};
