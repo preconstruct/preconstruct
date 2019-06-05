2dcce9710dee9547f8e01133bcc7e6eb8b8a5514
Jed Watson <jed.watson@me.com>
2019-06-06 00:12:45 +1000

    add github link to website homepage (#62)

--- site/src/fancy-link.js
+++ site/src/fancy-link.js
@@ -2,30 +2,31 @@ import React from "react";
 import { Link } from "gatsby";
 import colors from "./colors";
 
-export let FancyLink = props => (
+export let LinksContainer = props => (
   <div
     css={{ display: "flex", justifyContent: "center", alignItems: "center" }}
-  >
-    <Link
-      css={{
-        display: "block",
-        color: "inherit",
-        textDecoration: "none",
-        fontWeight: "bold",
-        paddingLeft: 32,
-        paddingRight: 32,
-        paddingTop: 8,
-        paddingBottom: 8,
-        marginLeft: -32,
-        marginRight: -32,
-        border: `${colors.base} solid 4px`,
-        borderRadius: 8,
-        ":hover": {
-          backgroundColor: colors.base,
-          color: "white"
-        }
-      }}
-      {...props}
-    />
-  </div>
+    {...props}
+  />
 );
+
+let fancyCSS = {
+  display: "block",
+  color: "inherit",
+  textDecoration: "none",
+  fontWeight: "bold",
+  paddingLeft: 32,
+  paddingRight: 32,
+  paddingTop: 8,
+  paddingBottom: 8,
+  marginLeft: 8,
+  marginRight: 8,
+  border: `${colors.base} solid 4px`,
+  borderRadius: 8,
+  ":hover": {
+    backgroundColor: colors.base,
+    color: "white"
+  }
+};
+
+export let FancyLink = props => <Link css={fancyCSS} {...props} />;
+export let FancyAnchor = props => <a css={fancyCSS} {...props} />;
