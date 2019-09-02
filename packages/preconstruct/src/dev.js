6b5294bb33616e6aaccfac3757f55e912065e63d
mitchellhamilton <mitchell@hamil.town>
2019-09-02 17:45:09 +1000

    Add comment to typescript redirect explaining what to do with default export problem

--- packages/preconstruct/src/dev.js
+++ packages/preconstruct/src/dev.js
@@ -94,12 +94,23 @@ async function writeTypeSystemFile(typeSystemPromise, entrypoint) {
     }
     await fs.writeFile(
       cjsDistPath + ".ts",
-      tsTemplate(
-        hasDefaultExport,
-        path
-          .relative(path.dirname(cjsDistPath), entrypoint.source)
-          .replace(/\.tsx?$/, "")
-      )
+      `// are you seeing an error that a default export doesn't exist but your source file has a default export?
+// you should run \`yarn\` or \`yarn preconstruct dev\` if preconstruct dev isn't in your postinstall hook
+
+// curious why you need to?
+// this file exists so that you can import from the entrypoint normally
+// except that it points to your source file and you don't need to run build constantly
+// which means we need to re-export all of the modules from your source file
+// and since export * doesn't include default exports, we need to read your source file
+// to check for a default export and re-export it if it exists
+// it's not ideal, but it works pretty well ¯\\_(ツ)_/¯
+` +
+        tsTemplate(
+          hasDefaultExport,
+          path
+            .relative(path.dirname(cjsDistPath), entrypoint.source)
+            .replace(/\.tsx?$/, "")
+        )
     );
   }
 }
