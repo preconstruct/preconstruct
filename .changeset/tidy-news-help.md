4b4e55859f5dc474f26a44716f9e258fe374837d
mitchellhamilton <mitchell@hamil.town>
2020-01-07 12:45:10 +1000

    Make Next use a custom Babel config even if it's in different higher directory than the Next site with `rootMode: 'upward-optional'`

---
+++ .changeset/tidy-news-help.md
@@ -0,0 +1,5 @@
+---
+"@preconstruct/next": major
+---
+
+Make Next use a custom Babel config even if it's in different higher directory than the Next site with `rootMode: 'upward-optional'`
--- packages/next/index.js
+++ packages/next/index.js
@@ -2,11 +2,27 @@ module.exports = (nextConfig = {}) => {
   let originalWebpack = nextConfig.webpack;
 
   nextConfig.webpack = (webpackConfig, options) => {
-    webpackConfig.module.rules.push({
-      test: /\.+(js|jsx|ts|tsx)$/,
-      use: options.defaultLoaders.babel,
-      exclude: modulePath => /node_modules/.test(modulePath)
+    let hasFoundRule = false;
+    options.defaultLoaders.babel.options.rootMode = "upward-optional";
+    debugger;
+    webpackConfig.module.rules.forEach(rule => {
+      if (
+        rule.use === options.defaultLoaders.babel ||
+        (Array.isArray(rule.use) &&
+          rule.use.includes(options.defaultLoaders.babel))
+      ) {
+        hasFoundRule = true;
+        debugger;
+        delete rule.include;
+        debugger;
+      }
     });
+    if (!hasFoundRule) {
+      throw new Error(
+        "If you see this error, please open an issue with your Next.js version and @preconstruct/next version. The Next Babel loader could not be found"
+      );
+    }
+    debugger;
     return originalWebpack
       ? originalWebpack(webpackConfig, options)
       : webpackConfig;
