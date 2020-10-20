43adf6d1a9c53b1d7890ceecf5709b62cbbbe419
mitchellhamilton <mitchell@hamil.town>
2020-10-20 22:10:25 +1000

    Update snapshot

--- packages/cli/src/build/__tests__/other.ts
+++ packages/cli/src/build/__tests__/other.ts
@@ -211,23 +211,24 @@ test("typescript", async () => {
     var path = require('path');
 
     function _interopNamespace(e) {
-    	if (e && e.__esModule) return e;
-    	var n = Object.create(null);
-    	if (e) {
-    		Object.keys(e).forEach(function (k) {
-    			if (k !== 'default') {
-    				var d = Object.getOwnPropertyDescriptor(e, k);
-    				Object.defineProperty(n, k, d.get ? d : {
-    					enumerable: true,
-    					get: function () {
-    						return e[k];
-    					}
-    				});
-    			}
-    		});
+    	if (e && e.__esModule) { return e; } else {
+    		var n = Object.create(null);
+    		if (e) {
+    			Object.keys(e).forEach(function (k) {
+    				if (k !== 'default') {
+    					var d = Object.getOwnPropertyDescriptor(e, k);
+    					Object.defineProperty(n, k, d.get ? d : {
+    						enumerable: true,
+    						get: function () {
+    							return e[k];
+    						}
+    					});
+    				}
+    			});
+    		}
+    		n['default'] = e;
+    		return Object.freeze(n);
     	}
-    	n['default'] = e;
-    	return Object.freeze(n);
     }
 
     var path__namespace = /*#__PURE__*/_interopNamespace(path);
