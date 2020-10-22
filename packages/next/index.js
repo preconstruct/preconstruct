15a5bd806a6928542df6cf140bdc9be378b33ea2
Mitchell Hamilton <mitchell@hamil.town>
2020-10-22 13:36:25 +1000

    Fix issue with @preconstruct/next (#335)
    * WIP
    
    * Add a changeset
    
    * Revert a change
    
    * Fix a missing require

--- packages/next/index.js
+++ packages/next/index.js
@@ -1,3 +1,6 @@
+const resolve = require("resolve");
+const path = require("path");
+
 module.exports = (nextConfig = {}) => {
   let originalWebpack = nextConfig.webpack;
 
@@ -19,6 +22,52 @@ module.exports = (nextConfig = {}) => {
         "If you see this error, please open an issue with your Next.js version and @preconstruct/next version. The Next Babel loader could not be found"
       );
     }
+    const { extensions } = webpackConfig.resolve;
+    if (options.isServer && !options.isServerless) {
+      let externalsFunc = webpackConfig.externals[0];
+      let makeCallback = (callback, context, request) => (err, result) => {
+        if (err) {
+          return callback(err);
+        }
+        if (result === undefined) {
+          return callback();
+        }
+        if (
+          result === "commonjs next" ||
+          result.startsWith(`commonjs next${path.posix.sep}`) ||
+          result.startsWith(`commonjs next${path.win32.sep}`)
+        ) {
+          return callback(undefined, result);
+        }
+
+        const resolved = resolve.sync(request, {
+          basedir: context,
+          extensions,
+          preserveSymlinks: false,
+        });
+        if (resolved.includes("node_modules")) {
+          return callback(undefined, request);
+        }
+        callback();
+      };
+      webpackConfig.externals = [
+        externalsFunc.length === 2
+          ? ({ context, request }, callback) => {
+              externalsFunc(
+                { context, request },
+                makeCallback(callback, context, request)
+              );
+            }
+          : (context, request, callback) => {
+              externalsFunc(
+                context,
+                request,
+                makeCallback(callback, context, request)
+              );
+            },
+      ];
+    }
+
     return originalWebpack
       ? originalWebpack(webpackConfig, options)
       : webpackConfig;
