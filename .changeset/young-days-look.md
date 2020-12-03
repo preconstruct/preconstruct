c2a9918ee73f5900f6c5b9a96c2efd94299ab416
mitchellhamilton <mitchell@hamil.town>
2020-12-03 10:32:29 +1000

    Fix entrypoint glob check on Windows

---
+++ .changeset/young-days-look.md
@@ -0,0 +1,5 @@
+---
+"@preconstruct/cli": patch
+---
+
+Fix entrypoint glob check on Windows
--- packages/cli/src/glob-thing.ts
+++ packages/cli/src/glob-thing.ts
@@ -1,19 +1,20 @@
 import parseGlob from "parse-glob";
 import * as fs from "fs-extra";
 import path from "path";
+import normalizePath from "normalize-path";
 
 export async function getUselessGlobsThatArentReallyGlobsForNewEntrypoints(
   globs: string[],
   files: string[],
   cwd: string
 ) {
-  let filesSet = new Set(files);
+  let filesSet = new Set(files.map((x) => normalizePath(x)));
   return (
     await Promise.all(
       globs.map(async (glob) => {
         let parsedGlobResult = parseGlob(glob);
         if (!parsedGlobResult.is.glob) {
-          let filename = path.resolve(cwd, "src", glob);
+          let filename = normalizePath(path.resolve(cwd, "src", glob));
           if (filesSet.has(filename)) return;
           try {
             await fs.stat(filename);
