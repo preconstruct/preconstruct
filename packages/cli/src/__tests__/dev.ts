6a33c0b40a12358a9c1849dc3ae11994145b41d1
Emma Hamilton <git@emmas.town>
2023-03-30 09:47:49 +1000

    Update snapshots

--- packages/cli/src/__tests__/dev.ts
+++ packages/cli/src/__tests__/dev.ts
@@ -274,16 +274,6 @@ test("typescript", async () => {
   expect(await getFiles(tmpPath, ["**/*.d.ts{,.map}", "!node_modules/**"]))
     .toMatchInlineSnapshot(`
     ⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯ dist/typescript.cjs.d.ts ⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯
-    // are you seeing an error that a default export doesn't exist but your source file has a default export?
-    // you should run \`yarn\` or \`yarn preconstruct dev\` if preconstruct dev isn't in your postinstall hook
-
-    // curious why you need to?
-    // this file exists so that you can import from the entrypoint normally
-    // except that it points to your source file and you don't need to run build constantly
-    // which means we need to re-export all of the modules from your source file
-    // and since export * doesn't include default exports, we need to read your source file
-    // to check for a default export and re-export it if it exists
-    // it's not ideal, but it works pretty well ¯\\_(ツ)_/¯
     export * from "../src/index";
     export { default } from "../src/index";
     //# sourceMappingURL=typescript.cjs.d.ts.map
@@ -362,16 +352,6 @@ test("flow and .d.ts", async () => {
   const files = await getFiles(tmpPath, ["dist/**", "!dist/pkg.cjs.js"]);
   expect(files).toMatchInlineSnapshot(`
     ⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯ dist/pkg.cjs.d.ts ⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯
-    // are you seeing an error that a default export doesn't exist but your source file has a default export?
-    // you should run \`yarn\` or \`yarn preconstruct dev\` if preconstruct dev isn't in your postinstall hook
-
-    // curious why you need to?
-    // this file exists so that you can import from the entrypoint normally
-    // except that it points to your source file and you don't need to run build constantly
-    // which means we need to re-export all of the modules from your source file
-    // and since export * doesn't include default exports, we need to read your source file
-    // to check for a default export and re-export it if it exists
-    // it's not ideal, but it works pretty well ¯\\_(ツ)_/¯
     export * from "../src/index.js";
     //# sourceMappingURL=pkg.cjs.d.ts.map
 
