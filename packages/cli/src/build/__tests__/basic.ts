42a851d7d56781fe7cfe6961ba37a60cc3a35744
prastoin <paul@twenty.com>
2025-03-21 11:30:23 +0100

    tests(basic): mono and standalone package custom config file

--- packages/cli/src/build/__tests__/basic.ts
+++ packages/cli/src/build/__tests__/basic.ts
@@ -1293,3 +1293,225 @@ test(".d.ts file with default export", async () => {
 
   `);
 });
+
+test("typescript with custom tsconfig filename in standalone package", async () => {
+  let tmpPath = await testdir({
+    "package.json": JSON.stringify({
+      name: "typescript-custom-config",
+      main: "dist/typescript-custom-config.cjs.js",
+      module: "dist/typescript-custom-config.esm.js",
+      dependencies: {
+        "@babel/runtime": "^7.8.7",
+      },
+      devDependencies: {
+        typescript: "^3.8.3",
+      },
+      preconstruct: {
+        entrypoints: ["index.ts"],
+        typescript: {
+          tsConfigFilename: "tsconfig.build.json",
+        },
+      },
+    }),
+    ".babelrc": JSON.stringify({
+      presets: [require.resolve("@babel/preset-typescript")],
+    }),
+    node_modules: {
+      kind: "symlink",
+      path: repoNodeModules,
+    },
+    "tsconfig.build.json": JSON.stringify({
+      compilerOptions: {
+        module: "commonjs",
+        moduleResolution: "node",
+        jsx: "react",
+        declaration: true,
+        isolatedModules: true,
+        strict: true,
+        esModuleInterop: true,
+        noEmit: true,
+      },
+    }),
+    "src/index.ts": ts`
+      export const thing = "custom-config" as const;
+    `,
+  });
+  await build(tmpPath);
+  await snapshotDirectory(path.join(tmpPath, "dist"), { files: "all" });
+});
+
+test("typescript with invalid custom tsconfig filename in standalone package", async () => {
+  let tmpPath = await testdir({
+    "package.json": JSON.stringify({
+      name: "typescript-invalid-custom-config",
+      main: "dist/typescript-invalid-custom-config.cjs.js",
+      module: "dist/typescript-invalid-custom-config.esm.js",
+      dependencies: {
+        "@babel/runtime": "^7.8.7",
+      },
+      devDependencies: {
+        typescript: "^3.8.3",
+      },
+      preconstruct: {
+        entrypoints: ["index.ts"],
+        typescript: {
+          tsConfigFilename: "tsconfig.invalid.json",
+        },
+      },
+    }),
+    ".babelrc": JSON.stringify({
+      presets: [require.resolve("@babel/preset-typescript")],
+    }),
+    node_modules: {
+      kind: "symlink",
+      path: repoNodeModules,
+    },
+    "tsconfig.build.json": JSON.stringify({
+      compilerOptions: {
+        target: "esnext",
+        module: "esnext",
+        jsx: "react",
+        isolatedModules: true,
+        strict: true,
+        moduleResolution: "node",
+        esModuleInterop: true,
+        noEmit: true,
+      },
+      "tsconfig.json": JSON.stringify({
+        compilerOptions: {
+          module: "nodenext",
+          moduleResolution: "nodenext",
+          jsx: "react",
+          declaration: true,
+          isolatedModules: true,
+          strict: true,
+          esModuleInterop: true,
+          noEmit: true,
+        },
+      }),
+    }),
+    "src/index.ts": ts`
+      export const thing = "custom-invalid-config" as const;
+    `,
+  });
+  await expect(build(tmpPath)).rejects.toMatchInlineSnapshot(
+    `[Error: 🎁 typescript-invalid-custom-config an entrypoint source file ends with the .ts or tsx extension but no TypeScript config exists, please create one.]`
+  );
+});
+
+test("typescript with custom tsconfig filename in monorepo package", async () => {
+  let tmpPath = await testdir({
+    "package.json": JSON.stringify({
+      name: "monorepo-root",
+      private: true,
+      workspaces: ["packages/*"],
+      devDependencies: {
+        typescript: "^3.8.3",
+      },
+      preconstruct: {
+        packages: ["packages/*"],
+      },
+    }),
+    ".babelrc": JSON.stringify({
+      presets: [require.resolve("@babel/preset-typescript")],
+    }),
+    node_modules: {
+      kind: "symlink",
+      path: repoNodeModules,
+    },
+    "packages/pkg-a/package.json": JSON.stringify({
+      name: "pkg-a",
+      main: "dist/pkg-a.cjs.js",
+      module: "dist/pkg-a.esm.js",
+      preconstruct: {
+        typescript: {
+          entrypoints: ["index.ts"],
+          tsConfigFilename: "tsconfig.build.json",
+        },
+      },
+    }),
+    "packages/pkg-a/tsconfig.build.json": JSON.stringify({
+      compilerOptions: {
+        module: "commonjs",
+        jsx: "react",
+        declaration: true,
+        isolatedModules: true,
+        strict: true,
+        moduleResolution: "node",
+        esModuleInterop: true,
+        noEmit: true,
+      },
+    }),
+    "packages/pkg-a/src/index.ts": ts`
+      export const thing = "monorepo-custom-config";
+    `,
+  });
+  await build(tmpPath);
+  await snapshotDirectory(path.join(tmpPath, "packages/pkg-a/dist"), {
+    files: "all",
+  });
+});
+
+test("typescript with custom invalid tsconfig filename in monorepo package", async () => {
+  let tmpPath = await testdir({
+    "package.json": JSON.stringify({
+      name: "monorepo-root",
+      private: true,
+      workspaces: ["packages/*"],
+      devDependencies: {
+        typescript: "^3.8.3",
+      },
+      preconstruct: {
+        packages: ["packages/*"],
+      },
+    }),
+    ".babelrc": JSON.stringify({
+      presets: [require.resolve("@babel/preset-typescript")],
+    }),
+    node_modules: {
+      kind: "symlink",
+      path: repoNodeModules,
+    },
+    "packages/pkg-a/package.json": JSON.stringify({
+      name: "pkg-a",
+      main: "dist/pkg-a.cjs.js",
+      module: "dist/pkg-a.esm.js",
+      preconstruct: {
+        typescript: {
+          entrypoints: ["index.ts"],
+          tsConfigFilename: "tsconfig.invalid.json",
+        },
+      },
+    }),
+    "packages/pkg-a/tsconfig.json": JSON.stringify({
+      compilerOptions: {
+        target: "esnext",
+        module: "esnext",
+        jsx: "react",
+        isolatedModules: true,
+        strict: true,
+        moduleResolution: "node",
+        esModuleInterop: true,
+        noEmit: true,
+      },
+    }),
+    "packages/pkg-a/tsconfig.build.json": JSON.stringify({
+      compilerOptions: {
+        module: "commonjs",
+        moduleResolution: "node",
+        jsx: "react",
+        declaration: true,
+        isolatedModules: true,
+        strict: true,
+        esModuleInterop: true,
+        noEmit: true,
+      },
+    }),
+    "packages/pkg-a/src/index.ts": ts`
+      export const thing = "custom-invalid-config";
+    `,
+  });
+  await expect(build(tmpPath)).rejects.toMatchInlineSnapshot(
+    `[Error: 🎁 pkg-a an entrypoint source file ends with the .ts or tsx extension but no TypeScript config exists, please create one.]`
+  );
+});
